5ab20aff036edb44bb2ad8fcd1e6564bd9964402
Elliot Kang <kkang2097@gmail.com>
2023-10-15 05:16:48 -0700

    Create InputBox.tsx
    - need to rethink the Callback or something... hmm

---
+++ components/InputBox.tsx
@@ -0,0 +1,9 @@
+'use client'
+
+//TODO: On change, call the ChatEngine and update the list
+//Expand this later
+export const InputBox = ({inputCallback}: {inputCallback: Function}) => {
+
+
+    return <input className="input-box" type='textarea'></input>;
+}
