0f29dbf16dc233cf2c119ad8b7a5420b5a5b26a8
Elliot Kang <kkang2097@gmail.com>
2023-10-17 15:50:45 -0700

    callbacks

---
+++ app/frontend_callbacks/index.test.ts
@@ -0,0 +1,9 @@
+
+//Dummy test here.
+describe("Something",
+()=>{
+    test("to evaluate to ...", ()=> {
+        expect(1).toBe(1);
+    });
+
+});
--- app/frontend_callbacks/index.ts
+++ app/frontend_callbacks/index.ts
@@ -3,10 +3,22 @@
 
 //Normally, this would all be done in Axios.
 //But since this is NextJS, we can just use the Fetch API.
-
+import {ChatResponse} from 'llamaindex';
 
 
 
 export const doSomething = () => {
     return "sausage";
+}
+
+
+//Don't use this yet
+export const getQuery = async (query: string) : Promise<ChatResponse> => {
+    const response = await fetch(`http://localhost:3000/api/chat/route`,
+    {
+        method: 'POST',
+        body: JSON.stringify({query: query})
+    });
+    console.log("GOT RESPONSE");
+    return response.body;
 }
